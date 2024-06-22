a3604cfdb2d5bb480926774940e9da8315f6c81d
Adrián <adriancoroneltandil@gmail.com>
2024-06-22 19:52:23 -0300

    sumado un git ignore

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'Biblioteca'
 });
 
